17f714c627d63a6907bacbc848af563d956f286f
TTTheMaTTT <projectttower@gmail.com>
2016-01-31 12:34:32 +0300

    Have created a folder SomeMaterials. It can be usefull

--- Assets/Scripts/Special/Tags.cs
+++ Assets/Scripts/Special/Tags.cs
@@ -4,11 +4,12 @@ using System.Collections;
 /// <summary>
 /// Использовать сокращённые названия тегов.
 /// </summary>
-public class Tags : MonoBehaviour {
+public static class Tags {
 
 	public const string player = "Player";
 	public const string gameController="GameController";
 	public const string indicators="indicators";
+    
 
 
 }
